ad46d6dfc39e07b7723a5be687e473ca523fcf72
meysamzandy <zandy.meysam@gmail.com>
2020-09-22 15:47:50 +0330

    add market_access_limit migration

---
+++ app/Models/MarketAccessLimit.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MarketAccessLimit extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2020_09_22_120142_create_market_access_limits_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarketAccessLimitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('market_access_limits', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('code_id')->constrained('discount_codes')->onDelete('cascade');
+            $table->string('name', 40)->comment('market name');
+            $table->string('version_major', 3)->comment('market major version');
+            $table->string('version_minor', 3)->comment('market minor version');
+            $table->string('version_patch', 3)->comment('market patch version');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('market_access_limits');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+
+    }
+}
