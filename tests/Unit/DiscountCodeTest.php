b5dc2afc0e4ec42dee998acf94cfb0a3408baad3
meysamzandy <zandy.meysam@gmail.com>
2020-10-03 14:46:15 +0330

    add test for features

---
+++ tests/Unit/DiscountCodeTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Models;
+
+use App\Models\DiscountCode;
+use PHPUnit\Framework\TestCase;
+
+class DiscountCodeTest extends TestCase
+{
+
+    public function testInsertManualCode()
+    {
+
+    }
+
+    public function testCreateCode()
+    {
+
+    }
+
+    public function testUsers()
+    {
+
+    }
+
+    public function testMarket()
+    {
+
+    }
+
+    public function testInsertAutoCode()
+    {
+
+    }
+
+    public function testGroup()
+    {
+
+    }
+}
