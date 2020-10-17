b024c7b9cc2e35f3b0d15334cc743179ea071949
meysamzandy <zandy.meysam@gmail.com>
2020-10-17 12:46:44 +0330

    add test for group destroy code

--- tests/Feature/DiscountCodeGroupControllerTest.php
+++ tests/Feature/DiscountCodeGroupControllerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use App\Http\Controllers\DiscountCodeGroupController;
+use App\Http\Helper\JwtHelper;
+use App\Models\DiscountCodeGroups;
 use Illuminate\Support\Facades\Artisan;
 use Tests\TestCase;
 
@@ -59,6 +61,34 @@ class DiscountCodeGroupControllerTest extends TestCase
         self::assertEquals(1, $data['body']['current_page']);
         self::assertEquals(1, $data['body']['last_page']);
         self::assertEquals(1, $data['body']['total']);
+    }
+
+    public function testDestroy(): void
+    {
+        Artisan::call('migrate:refresh --seed --seeder=DiscountCodeGroupsSeeder');
+        $group = DiscountCodeGroups::all();
+        // check if wrong url
+        $url = 'self::GROUP_URL';
+        $response = $this->delete($url);
+        $response->assertStatus(404);
+
+        // check if token doesn't exist
+        $url = self::GROUP_URL. '/' . $group[0]['id'];
+        $response = $this->delete($url);
+        $response->assertStatus(403);
+
+
+        // delete group correctly
+        $url = self::GROUP_URL. '/' . $group[0]['id'];
+        $data = [
+            'password' => config('settings.admin_jwt.password')
+        ];
+        $jwt = JwtHelper::encodeJwt(config('settings.admin_jwt.key'), $data, 360000);
+        $response = $this->delete($url, [], ['token' => $jwt]);
+        $response->assertStatus(204);
+        $this->assertDatabaseMissing('discount_code_groups', [
+            'id' =>  $group[0]['id']
+        ]);
     }
 
 }
